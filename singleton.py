b5fd472feacb57120c805fbe10c0037a670f81b5
Andriy Yakubchuk <andruhache157@gmail.com>
2017-02-17 11:07:25 +0200

    Add files via upload

---
+++ singleton.py
@@ -0,0 +1,17 @@
+def singleton(cls):
+    instances = {}
+    def getinstance():
+        if cls not in instances:
+            instances[cls] = cls()
+        return instances[cls]
+    return getinstance
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+    def inc(self):
+        self.count += 1
+
+print (type(Counter))    # <type 'classobj'>
+Counter = singleton(Counter)
+print (type(Counter))
